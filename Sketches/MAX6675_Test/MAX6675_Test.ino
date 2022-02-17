3806958062b9ca88d03d2b8124a71f4e6e499ebe
TS200010 <tonyonbt@btinternet.com>
2022-02-17 18:41:42 +0000

    Added MAX6675 and LCD1602

---
+++ Sketches/MAX6675_Test/MAX6675_Test.ino
@@ -0,0 +1,78 @@
+/*
+ MAX6675 and LCD1692 Example
+
+
+    The circuit:
+ * MAX SCK pin to digital pin 6 -> 10
+ * MAX CS  pin to digital pin 5 -> 9
+ * MAX SO  pin to digital pin 4 -> 8 
+ * LCD RS  pin 4 to digital pin 12
+ * LCD En  pin 6 to digital pin 11
+ * LCD D4  pin 10 to digital pin 5
+ * LCD D5  pin 11 to digital pin 4
+ * LCD D6  pin 12 to digital pin 3
+ * LCD D7  pin 13 to digital pin 2
+ * LCD R/W pin 5 to ground
+ * LCD VSS pin 1 to ground
+ * LCD VCC pin 2 to 5V
+ * 10K potentiometer VR1:
+ * VR1 ends to +5V and ground
+ * LCD VO  pin 3 to VR1 wiper
+
+*/
+
+
+
+#include "max6675.h" // max6675.h file is part of the library that you should download from Robojax.com
+#include "math.h"
+#include <LiquidCrystal.h>
+
+int soPin  = 8;// SO=Serial Out
+int csPin  = 9;// CS = chip select CS pin
+int sckPin = 10;// SCK = Serial Clock pin
+float EMA=0;
+float alpha=.3;
+
+
+MAX6675 Module(sckPin, csPin, soPin);// create instance object of MAX6675
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {          
+  Serial.begin(9600);// initialize serial monitor with 9600 baud
+  Serial.println("MAX6675"); 
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD
+  lcd.print("EMA     Temp");
+
+
+}
+
+void loop() {
+   float t=Module.readCelsius();
+   if (isnan(EMA)) {EMA=0;};
+   EMA = alpha*t + (1-alpha) * EMA;
+   //Serial.print("EMA="); 
+   Serial.print(EMA); 
+   Serial.print(" ");
+   //Serial.print(" Temp="); 
+   Serial.print(t);
+   Serial.print(" ");
+   Serial.println("");
+   delay(500);
+   
+
+    // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  //lcd.print(millis() / 1000);
+  if (!isnan(EMA)) { lcd.print( EMA ); } else {lcd.print( "NoVal");};
+  lcd.print( "   " );
+  if (!isnan(t)) { lcd.print( t ); } else {lcd.print( "NoVal");};
+  lcd.print( "   " );
+}
