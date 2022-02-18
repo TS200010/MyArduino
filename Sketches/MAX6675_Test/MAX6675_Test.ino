5d5ce81b709de084eec7e1e2fee1e36a1025ebd7
TS200010 <tonyonbt@btinternet.com>
2022-02-18 18:32:29 +0000

    Switched to TryEnums

--- Sketches/MAX6675_Test/MAX6675_Test.ino
+++ Sketches/MAX6675_Test/MAX6675_Test.ino
@@ -21,17 +21,16 @@
 
 */
 
-
-
 #include "max6675.h" 
 #include "math.h"
 #include <LiquidCrystal.h>
+#include "AOFwk.h"
 
 const int soPin  = 8;   // SO=Serial Out
 const int csPin  = 9;   // CS = chip select CS pin
 const int sckPin = 10;  // SCK = Serial Clock pin
 float EMA=0;
-float alpha=0.3;
+float alpha=0.1;
 
 enum tState {tRising, tFalling, tAmbient, tSteady};
 
@@ -47,12 +46,22 @@ public:
 };
 
 
-MAX6675 Module(sckPin, csPin, soPin);// create instance object of MAX6675
+MAX6675 tSensor(sckPin, csPin, soPin);// create instance object of MAX6675
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Constants User can update to explore the behaviour of the example
+const uint8_t kPWMBrightnessMax = 10;           // Maximum PWM value controls maximum brightness ( 0 to kPWMMax )
+const int kTotalCycleTime = kTenSeconds;        // Number of milliseconds for the complete cycle of 
+                                                //   three colours. Note how the constants improve readability here.
+const int kLEDUpdateDelay = kTotalCycleTime / (kPWMBrightnessMax + 1); 
+
+CPWMPin  g_BlueLED(  kPWM3, kActiveHigh );
+CPWMPin  g_GreenLED( kPWM5, kActiveHigh );
+CPWMPin  g_RedLED(   kPWM6, kActiveHigh );
 
 void setup() {          
   Serial.begin(9600);// initialize serial monitor with 9600 baud
@@ -60,16 +69,16 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD
-  lcd.print("EMA     Temp (v1.1)");
+  lcd.print("EMA     Temp v1");
 }
 
 void loop() {
    int startms = millis();
 //   float t=Module.readCelsius();
    temperature t; 
-   t.SetTemp(Module.readCelsius());
-   if (isnan(EMA)) {EMA=0;};
+   t.SetTemp(tSensor.readCelsius());
    EMA = alpha*t.GetTemp() + (1-alpha) * EMA;
+   if (isnan(EMA)) {EMA=0;};   
    //Serial.print("EMA="); 
    Serial.print(EMA); 
    Serial.print(" ");
@@ -77,7 +86,7 @@ void loop() {
    Serial.print(t.GetTemp());
    Serial.print(" ");
    Serial.println("");
-   delay(500-millis()-startms);
+   delay(500-(millis()-startms));
    
 
   // set the cursor to column 0, line 1
